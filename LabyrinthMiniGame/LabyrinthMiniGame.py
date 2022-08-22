10b2db06b1983c477a82a7756d97525ae165374b
CaptivatingTime <111361451+CaptivatingTime@users.noreply.github.com>
2022-08-22 21:59:49 +0300

    Update

--- LabyrinthMiniGame/LabyrinthMiniGame.py
+++ LabyrinthMiniGame/LabyrinthMiniGame.py
@@ -1,17 +1,21 @@
+import sys, os
 
 
-map = [["|-|","-|","-|","-|","F|"],
-       ["|-|","-|","-|","-|","-|"],
-       ["|-|","-|","-|","-|","-|"],
-       ["|-|","-|","-|","-|","-|"],
-       ["|-|","-|","-|","-|","-|"],
-       ["|x|","-|","-|","-|","-|"]]
 
-def showMap():
-    for r in map:
-     for c in r:
-      print(c,end="")
-     print()
+class Map:
+    update = [["|-|","-|","-|","-|","F|"],
+      ["|-|","-|","-|","-|","-|"],
+      ["|-|","-|","-|","-|","-|"],
+      ["|-|","-|","-|","-|","-|"],
+      ["|-|","-|","-|","-|","-|"],
+      ["|x|","-|","-|","-|","-|"]]
+    def show():
+     for r in Map.update:
+      for c in r:
+       print(c,end="")
+      print()
+
+
 
 
 def choice2dr(dir1, dir2):
@@ -28,18 +32,18 @@ def mapUpdate (direction,cordX,cordY):
     
     if direction == "up":
         if cordY < 4:
-            map[cordX-1][cordY] = "|*|"
+            Map.update[cordX-1][cordY] = "|*|"
         else:
-            map[cordX-1][cordY] = "*|"
+            Map.update[cordX-1][cordY] = "*|"
        
     elif direction == "right":
-        map[cordX][cordY + 1] = "*|"
+        Map.update[cordX][cordY + 1] = "*|"
 
     elif direction == "down":
-        map[cordX + 1][cordY] = "*|"
+        Map.update[cordX + 1][cordY] = "*|"
 
     elif direction == "left":
-        map[cordX][cordY - 1] = "*"
+        Map.update[cordX][cordY - 1] = "*"
 
 
 
@@ -47,110 +51,162 @@ def removePath (direction,cordX,cordY):
     
     if direction == "up":
         if cordY < 4:
-            map[cordX-1][cordY] = "| |"
+            Map.update[cordX-1][cordY] = "| |"
         else:
-            map[cordX-1][cordY] = " |"
+            Map.update[cordX-1][cordY] = " |"
        
     elif direction == "right":
         if cordX == 2 and cordY == -1:
-            map[cordX][cordY + 1] = "|  "
+            Map.update[cordX][cordY + 1] = "|  "
         else:
-            map[cordX][cordY + 1] = "  "
+            Map.update[cordX][cordY + 1] = "  "
 
     elif direction == "down":
-        map[cordX + 1][cordY] = " |"
+        Map.update[cordX + 1][cordY] = " |"
 
     elif direction == "left":
-        map[cordX][cordY - 1] = "  |"
+        Map.update[cordX][cordY - 1] = "  |"
 
 
     
 class Move:
+    isSafe = True
     cordX = 5
     cordY = 0
     def up(decision, correct):
         if decision == correct:
             mapUpdate(decision, Move.cordX, Move.cordY)
             removePath(decision,Move.cordX + 1, Move.cordY)
-            showMap()
+            Map.show()
             Move.cordX = Move.cordX - 1
+        else:
+           Move.isSafe = False
             
 
     def right(decision, correct):
         if decision == correct:
             mapUpdate(decision, Move.cordX, Move.cordY)
             removePath(decision,Move.cordX, Move.cordY - 1)
-            showMap()
+            Map.show()
             Move.cordY = Move.cordY + 1
+        else:
+            Move.isSafe = False
 
     def down(decision, correct):
         if decision == correct:
             mapUpdate(decision, Move.cordX, Move.cordY)
             removePath(decision,Move.cordX - 1, Move.cordY)
-            showMap()
+            Map.show()
             Move.cordX = Move.cordX + 1
+        else:
+            Move.isSafe = False
 
     def left(decision, correct):
         if decision == correct:
             mapUpdate(decision, Move.cordX, Move.cordY)
             removePath(decision,Move.cordX, Move.cordY + 1)
-            showMap()
+            Map.show()
             Move.cordY = Move.cordY - 1
+        else:
+            Move.isSafe = False
             
         
+def restart():
+    Move.cordX = 5
+    Move.cordY = 0
+    Move.isSafe = True
+    Map.update = [["|-|","-|","-|","-|","F|"],
+      ["|-|","-|","-|","-|","-|"],
+      ["|-|","-|","-|","-|","-|"],
+      ["|-|","-|","-|","-|","-|"],
+      ["|-|","-|","-|","-|","-|"],
+      ["|x|","-|","-|","-|","-|"]]
 
 
-print(" You woke up in mysterious place.\n Everything around you seems familiar yet unknown.\n All you know is that you take one step at a time,\n if it is right one - good, if not - you are no more\n")
-
-for r in map:
- for c in r:
-  print(c,end="")
- print()
+print(" You woke up in mysterious place.\n Everything around you seems familiar yet unknown.\n All you know is that you take one step at a time,\n if it is right one - good, if not - you are out of lives.\n")
 
 
+again = True
+reason = "Wrong move,"
 
-choice = choice2dr("up","right")
-Move.up(choice, "up")
+while again == True:
+    try:
+        Map.show()
+        choice = choice2dr("up","right")
+        Move.up(choice, "up")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice2dr("up","right")
-Move.up(choice, "up")
+        choice = choice2dr("up","right")
+        Move.up(choice, "up")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice2dr("up","right")
-Move.up(choice, "up")
+        choice = choice2dr("up","right")
+        Move.up(choice, "up")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice2dr("up","right")
-Move.right(choice, "right")
+        choice = choice2dr("up","right")
+        Move.right(choice, "right")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice3dr("up","right", "down")
-Move.right(choice, "right")
+        choice = choice3dr("up","right", "down")
+        Move.right(choice, "right")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice3dr("up","right", "down")
-Move.down(choice, "down")
+        choice = choice3dr("up","right", "down")
+        Move.down(choice, "down")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice3dr("left","right", "down")
-Move.right(choice, "right")
+        choice = choice3dr("left","right", "down")
+        Move.right(choice, "right")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice2dr("up","down")
-Move.down(choice, "down")
+        choice = choice2dr("up","down")
+        Move.down(choice, "down")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice3dr("left","right", "down")
-Move.right(choice, "right")
+        choice = choice3dr("left","right", "down")
+        Move.right(choice, "right")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice3dr("up","right", "down")
-Move.up(choice, "up")
+        choice = choice3dr("up","right", "down")
+        Move.up(choice, "up")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice2dr("up","up")
-Move.up(choice, "up")
+        choice = choice2dr("up","up")
+        Move.up(choice, "up")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice2dr("up","left")
-Move.up(choice, "up")
+        choice = choice2dr("up","left")
+        Move.up(choice, "up")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice2dr("left","up")
-Move.left(choice, "left")    
+        choice = choice2dr("left","up")
+        Move.left(choice, "left") 
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice3dr("left","up", "down")
-Move.up(choice, "up")
+        choice = choice3dr("left","up", "down")
+        Move.up(choice, "up")
+        if Move.isSafe != True: sys.exit(1)
 
-choice = choice2dr("left","right")
-Move.right(choice, "right")
+        choice = choice2dr("left","right")
+        Move.right(choice, "right")
+        if Move.isSafe != True: sys.exit(1)
 
+        print("\nYou actualy did it!\n")
+        print("|-|-|-|$|$|\n\
+|-|-|-|$|$|\n\
+|$|$|$|-|$|\n\
+|$|-|$|$|$|\n\
+|$|-|-|$|$|\n\
+|x|-|-|-|-|\n")
+        Move.isSafe = False
+        reason = "You did it! "
+        if Move.isSafe != True: sys.exit(1)
+  
+    except:
+       yorn = input(reason + "Try again? (y/n)\n")
+       if yorn == "y":
+           restart()
+       else:
+           again = False
+           print("We will meet soon enough again!")
