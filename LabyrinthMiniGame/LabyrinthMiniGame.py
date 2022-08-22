cc2ef5bb7073d650e748da9e9010949b3bbdb856
CaptivatingTime <111361451+CaptivatingTime@users.noreply.github.com>
2022-08-22 19:19:14 +0300

    array implemented

--- LabyrinthMiniGame/LabyrinthMiniGame.py
+++ LabyrinthMiniGame/LabyrinthMiniGame.py
@@ -1,213 +1,157 @@
 
-step = 1
-def choice2d(dir1, dir2):
+
+map = [["|-|","-|","-|","-|","F|"],
+       ["|-|","-|","-|","-|","-|"],
+       ["|-|","-|","-|","-|","-|"],
+       ["|-|","-|","-|","-|","-|"],
+       ["|-|","-|","-|","-|","-|"],
+       ["|x|","-|","-|","-|","-|"]]
+
+def showMap():
+    for r in map:
+     for c in r:
+      print(c,end="")
+     print()
+
+
+def choice2dr(dir1, dir2):
     decision = input ("You have two choses - " + dir1 + " or " + dir2 + "." + " Chose!\n ")
     return decision
 
-def choice3d(dir1, dir2, dir3):
-    decision = input ("You have two choses - " + dir1 + ", " + dir2 + " or " + dir3 + "." + " Chose!\n ")
+def choice3dr(dir1, dir2, dir3):
+    decision = input ("You have three choses - " + dir1 + ", " + dir2 + " or " + dir3 + "." + " Chose!\n ")
     return decision
 
-def mapUpdate (steps):
-    
-    
-    if steps == 1:
-        print("\nGood you live.....for now\n")
-        print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-|-|-|-|-|-|\n\
-|-|-|-|-|-|\n\
-|*|-|-|-|-|\n\
-|x|-|-|-|-|\n")
-    elif steps == 2:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-|-|-|-|-|-|\n\
-|*|-|-|-|-|\n\
-| |-|-|-|-|\n\
-|x|-|-|-|-|\n")
-    elif steps == 3:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-|*|-|-|-|-|\n\
-| |-|-|-|-|\n\
-| |-|-|-|-|\n\
-|x|-|-|-|-|\n")
-    elif steps == 4:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-| |*|-|-|-|\n\
-| |-|-|-|-|\n\
-| |-|-|-|-|\n\
-|x|-|-|-|-|\n")
-    elif steps == 5:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-| | |*|-|-|\n\
-| |-|-|-|-|\n\
-| |-|-|-|-|\n\
-|x|-|-|-|-|\n")
-    elif steps == 6:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-| | | |-|-|\n\
-| |-|*|-|-|\n\
-| |-|-|-|-|\n\
-|x|-|-|-|-|\n")
-    elif steps == 7:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-| | | |-|-|\n\
-| |-| |*|-|\n\
-| |-|-|-|-|\n\
-|x|-|-|-|-|\n")
-    elif steps == 8:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-| | | |-|-|\n\
-| |-| | |-|\n\
-| |-|-|*|-|\n\
-|x|-|-|-|-|\n")
-    elif steps == 9:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-| | | |-|-|\n\
-| |-| | |-|\n\
-| |-|-| |*|\n\
-|x|-|-|-|-|\n")
-    elif steps == 10:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-| | | |-|-|\n\
-| |-| | |*|\n\
-| |-|-| | |\n\
-|x|-|-|-|-|\n")
-    elif steps == 11:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-| | | |-|*|\n\
-| |-| | | |\n\
-| |-|-| | |\n\
-|x|-|-|-|-|\n")
-    elif steps == 12:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|-|*|\n\
-| | | |-| |\n\
-| |-| | | |\n\
-| |-|-| | |\n\
-|x|-|-|-|-|\n")
-    elif steps == 13:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|-|F|\n\
-|-|-|-|*| |\n\
-| | | |-| |\n\
-| |-| | | |\n\
-| |-|-| | |\n\
-|x|-|-|-|-|\n")
-    elif steps == 14:
-          print("\nGood you live.....for now\n")
-          print("|-|-|-|*|F|\n\
-|-|-|-| | |\n\
-| | | |-| |\n\
-| |-| | | |\n\
-| |-|-| | |\n\
-|x|-|-|-|-|\n")
-    elif steps == 15:
-          print("\nYou actualy did it!\n")
-          print("|-|-|-|$|$|\n\
-|-|-|-|$|$|\n\
-|$|$|$|-|$|\n\
-|$|-|$|$|$|\n\
-|$|-|-|$|$|\n\
-|x|-|-|-|-|\n")
 
 
-def isCorrect(decision, correct):
-    if decision == correct:
-        mapUpdate(step)
+def mapUpdate (direction,cordX,cordY):
+    
+    if direction == "up":
+        if cordY < 4:
+            map[cordX-1][cordY] = "|*|"
+        else:
+            map[cordX-1][cordY] = "*|"
+       
+    elif direction == "right":
+        map[cordX][cordY + 1] = "*|"
+
+    elif direction == "down":
+        map[cordX + 1][cordY] = "*|"
+
+    elif direction == "left":
+        map[cordX][cordY - 1] = "*"
+
+
+
+def removePath (direction,cordX,cordY):
+    
+    if direction == "up":
+        if cordY < 4:
+            map[cordX-1][cordY] = "| |"
+        else:
+            map[cordX-1][cordY] = " |"
+       
+    elif direction == "right":
+        if cordX == 2 and cordY == -1:
+            map[cordX][cordY + 1] = "|  "
+        else:
+            map[cordX][cordY + 1] = "  "
+
+    elif direction == "down":
+        map[cordX + 1][cordY] = " |"
+
+    elif direction == "left":
+        map[cordX][cordY - 1] = "  |"
+
+
+    
+class Move:
+    cordX = 5
+    cordY = 0
+    def up(decision, correct):
+        if decision == correct:
+            mapUpdate(decision, Move.cordX, Move.cordY)
+            removePath(decision,Move.cordX + 1, Move.cordY)
+            showMap()
+            Move.cordX = Move.cordX - 1
+            
+
+    def right(decision, correct):
+        if decision == correct:
+            mapUpdate(decision, Move.cordX, Move.cordY)
+            removePath(decision,Move.cordX, Move.cordY - 1)
+            showMap()
+            Move.cordY = Move.cordY + 1
+
+    def down(decision, correct):
+        if decision == correct:
+            mapUpdate(decision, Move.cordX, Move.cordY)
+            removePath(decision,Move.cordX - 1, Move.cordY)
+            showMap()
+            Move.cordX = Move.cordX + 1
+
+    def left(decision, correct):
+        if decision == correct:
+            mapUpdate(decision, Move.cordX, Move.cordY)
+            removePath(decision,Move.cordX, Move.cordY + 1)
+            showMap()
+            Move.cordY = Move.cordY - 1
+            
         
 
 
 print(" You woke up in mysterious place.\n Everything around you seems familiar yet unknown.\n All you know is that you take one step at a time,\n if it is right one - good, if not - you are no more\n")
 
-print("|-|-|-|-|F|\n\
-|-|-|-|-|-|\n\
-|-|-|-|-|-|\n\
-|-|-|-|-|-|\n\
-|-|-|-|-|-|\n\
-|x|-|-|-|-|\n")
-
-step = 1
+for r in map:
+ for c in r:
+  print(c,end="")
+ print()
 
 
-choice = choice2d("up","right")
-isCorrect(choice, "up")
-step = step + 1
 
-choice = choice2d("up","right")
-isCorrect(choice, "up")
-step = step + 1
+choice = choice2dr("up","right")
+isSafe(choice, "up")
+Move.up(choice, "up")
 
-choice = choice2d("up","right")
-isCorrect(choice, "up")
-step = step + 1
+choice = choice2dr("up","right")
+Move.up(choice, "up")
 
-choice = choice2d("up","right")
-isCorrect(choice, "right")
-step = step + 1
+choice = choice2dr("up","right")
+Move.up(choice, "up")
 
-choice = choice3d("up","right", "down")
-isCorrect(choice, "right")
-step = step + 1
+choice = choice2dr("up","right")
+Move.right(choice, "right")
 
-choice = choice3d("up","right", "down")
-isCorrect(choice, "down")
-step = step + 1
+choice = choice3dr("up","right", "down")
+Move.right(choice, "right")
 
-choice = choice3d("left","right", "down")
-isCorrect(choice, "right")
-step = step + 1
+choice = choice3dr("up","right", "down")
+Move.down(choice, "down")
 
-choice = choice2d("up","down")
-isCorrect(choice, "down")
-step = step + 1
+choice = choice3dr("left","right", "down")
+Move.right(choice, "right")
 
-choice = choice3d("left","right", "down")
-isCorrect(choice, "right")
-step = step + 1
+choice = choice2dr("up","down")
+Move.down(choice, "down")
 
-choice = choice3d("up","right", "down")
-isCorrect(choice, "up")
-step = step + 1
+choice = choice3dr("left","right", "down")
+Move.right(choice, "right")
 
-choice = choice2d("up","up")
-isCorrect(choice, "up")
-step = step + 1
+choice = choice3dr("up","right", "down")
+Move.up(choice, "up")
 
-choice = choice2d("up","left")
-isCorrect(choice, "up")
-step = step + 1
+choice = choice2dr("up","up")
+Move.up(choice, "up")
 
-choice = choice2d("left","left")
-isCorrect(choice, "left")
-step = step + 1       
+choice = choice2dr("up","left")
+Move.up(choice, "up")
 
-choice = choice3d("left","up", "down")
-isCorrect(choice, "up")
-step = step + 1
+choice = choice2dr("left","up")
+Move.left(choice, "left")    
 
-choice = choice2d("left","right")
-isCorrect(choice, "right")
-step = step + 1 
+choice = choice3dr("left","up", "down")
+Move.up(choice, "up")
+
+choice = choice2dr("left","right")
+Move.right(choice, "right")
 
